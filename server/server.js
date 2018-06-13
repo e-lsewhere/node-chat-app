2e77e643c9603bbfb63fbc52bf3a94cd2866e5c5
user_name <miguel.gray95@gmail.com>
2018-06-13 18:08:16 -0400

    add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,30 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Max',
+    text: 'See YA BOI',
+    createdAt: 123445
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('Client Disconnected');
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 server.listen(port, () => {
